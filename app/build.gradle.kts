f63a96d5a779264d2dac58ef4412121c4e04dcd6
badnng <wsj316@outlook.com>
2026-03-21 05:29:07 +0800

    更改版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.Badnng.moe"
         minSdk = 35
         targetSdk = 36
-        versionCode = 20260320_11
-        versionName = "26.3.20.C01-Dev"
+        versionCode = 20260321_11
+        versionName = "26.3.21.C01-Dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
