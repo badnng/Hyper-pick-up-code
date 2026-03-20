12a84ae0df6e191fdef55aa0c4eba8cd382a5c7d
badnng <wsj316@outlook.com>
2026-03-21 01:25:16 +0800

    修改源

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,10 +20,10 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenCentral()
+        google()
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
-        google()
-        mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
 }
